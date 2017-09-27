c72d28ad19c8f8366ab2e6ce7f85fa210e6698da
nhatvm <nhatvuminh@admicro.vn>
2017-09-27 15:03:30 +0700

    first commit

---
+++ running-wordcount.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+MAIN_CLASS=$1
+MAIN_JAR=target/wordcount-0.0.1-SNAPSHOT.jar
+JARS=$(echo target/dependencies/*.jar | tr ' ' ',')
+spark-submit --master yarn-client --jars $JARS \
+	--num-executors 30 --executor-memory 1g --executor-cores 1 \
+--class $MAIN_CLASS $MAIN_JAR $2 $3 $4
---
+++ src/main/java/wordcount/WordCount.java
@@ -0,0 +1,30 @@
+package wordcount;
+
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class WordCount {
+	public static void main(String[] args) {
+		String dataFile = args[0];
+		String resultFolder = args[1];
+//		String dataFile = "/home/minhnhat/VCCorp/test.txt";
+//		String resultFolder = "/home/minhnhat/VCCorp/result";
+		SparkConf conf = new SparkConf().setAppName("My App").setMaster("local");
+		conf.set("spark.executor.memory", "10g");
+		conf.set("spark.cores.max", (Runtime.getRuntime().availableProcessors() - 1) + "");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> textFile = sc.textFile(dataFile);
+		JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+		        .mapToPair(word -> new Tuple2<String, Integer>(word, 1)).reduceByKey((a, b) -> a + b);
+
+		counts.saveAsTextFile(resultFolder);
+		System.out.println(counts.collect());
+		sc.close();
+	}
+}
